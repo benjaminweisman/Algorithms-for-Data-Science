ff7464fac92811cf1f2701c2b2f3b63802ef7fcc
Benjamin Weisman <benjamin.weisman15@ncf.edu>
2020-09-11 23:30:15 -0400

    Problem 2 and 3 Implementation

--- Assignment Two - Asymptotics and Plotting/Asymptotics-Plotting.py
+++ Assignment Two - Asymptotics and Plotting/Asymptotics-Plotting.py
@@ -19,22 +19,110 @@ def script(arr):
                 Swap(arr, k, k-1)
     return arr
 
+#test these individually after writing them
+
+def problem2a(arr):
+    ar=[]
+    for x in arr:  
+        x=4*(x**2)+x
+        ar.append(x)
+    return ar
+
+def problem2b(arr):
+    ar=[]
+    for x in arr:
+        x=64*x * np.log2(x) + 2*x
+        ar.append(x)
+    return ar
+
+
+
+def problem3a(arr):
+    ar=[]
+    for x in arr:
+        x=x**2
+        ar.append(x)
+    return ar
+
+def problem3b(arr):
+    ar=[]
+    for x in arr:
+        x=np.sqrt(x)
+        ar.append(x)
+    return ar
+
+def problem3c(arr):
+    ar=[]
+    for x in arr:
+        x=2**x
+        ar.append(x)
+    return ar
+
+def problem3d(arr):
+    ar=[]
+    for x in arr:
+        x=np.log2(x)
+        ar.append(x)
+    return ar
+
+def problem3e(arr):
+    ar=[]
+    for x in arr:
+        x=.5**x
+        ar.append(x)
+    return ar
+
+def problem3f(arr):
+    ar=[]
+    for x in arr:
+        x=x*np.log2(x)
+        ar.append(x)
+    return ar
+
+def problem3g(arr):
+    ar=[]
+    for x in arr:
+        x=x
+        ar.append(x)
+    return ar
+
+def problem3h(arr):
+    ar=[]
+    for x in arr:
+        x=x**3
+        ar.append(x)
+    return ar
 
 print(A)
+print(problem3h(A))
+    
 
 
 #list of N's
 seq = [x for x in range(20, 801) if x % 20 == 0]
 times=[]
+times2=[]
+
+new1=[]
+new2=[]
+timings = []
+timings2=[]
 
 #TYPE ALGORITHM HERE
-algorithm = script
+algorithm = problem2a
+#IF USING PAIR, TRUE
+pair = True
+if pair==True:
+    #TYPE ALGORITHM 2 HERE
+    algorithm2 = problem2b
+#DIMENSIONS OF ARRAY
 dimensions = 1
-print("Algorithm: ", str(algorithm))
+
+
 
 #returns time to run algorithm in milliseconds
 def timer(algo, timings, N, dim):
-    R=1
+    R=1000
     for i in range(R):
         if dim == 2:
             rands = np.random.rand(N,10)
@@ -48,17 +136,59 @@ def timer(algo, timings, N, dim):
         #print(timings)
     return((sum(timings))/R)
 
-timings = []
 
+
+
+
+def pairtimer(algo, algo2, timings, N, dim):
+    R=100
+    for i in range(R):
+        if dim == 2:
+            rands = np.random.rand(N,10)
+        else:
+            rands = np.random.rand(N)
+        start = time.time()
+        algo(rands)
+        end = time.time()
+        timings.append(end-start)
+        start2 = time.time()
+        algo2(rands)
+        end2=time.time()
+        timings2.append(end2-start2)
+    new1.append(sum(timings)/R)
+    new2.append(sum(timings2)/R)
+    
+
+t=0
 for i in seq:
-    print("For N of" , i,": ")
-    times.append(timer(algorithm, timings, i, dimensions))
-    print(timer(algorithm, timings, i, dimensions), "milliseconds")
+    print("For N of" , i)
+    if pair == True:
+        pairtimer(algorithm,algorithm2, timings, i, dimensions)
+        t=t+1
+    else:
+        times.append(timer(algorithm, timings, i, dimensions))
+        t=t+1
 
-pplot.plot(seq, np.array(times), label = str(algorithm))
+if pair == True:
+    pplot.plot(seq, np.array(new1), label = str(algorithm))
+    pplot.plot(seq, np.array(new2), label = str(algorithm2))
+    print("Intersecting on: ")
+    print(np.intersect1d(new1,new2))
+else:
+    pplot.plot(seq, np.array(times), label = str(algorithm))
+
+
+'''
 deltas = np.diff(times)
 print("Deltas between each N")
 print(deltas)
-pplot.show()
+'''
 
+pplot.legend()
+pplot.show()
 quit()
+
+
+
+
+
